17cb549b2a30e1aa674343dbb9b33ae622be3ab0
AlexaDoesStuff <alexadoestuff@protonmail.com>
2026-04-17 22:51:31 -0400

    Updating portfolio

--- src/pages/aboutme2.tsx
+++ src/pages/aboutme2.tsx
@@ -4,7 +4,7 @@ import '../components/components.scss';
 import portfolioVideo from '../assets/videos/portfoliovideo_warped.mov';
 import { StylizedMediaPlayer } from '../components/stylizedMediaPlayer';
 
-import portfolioPDF from '../assets/content/AlexaJ_Digital Portfolio.pdf';
+import portfolioPDF from '../assets/content/A4 - Alexa J Portfolio.pdf';
 
 const AboutMeAlternativePage = () => {
   return (
